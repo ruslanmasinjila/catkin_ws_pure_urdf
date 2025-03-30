a936d1e867a8a995a64ed3d9b16a36d0e5656e48
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2025-03-30 04:59:33 -0230

    initial commit

---
+++ build/xarm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ruslan/catkin_ws/src/xarm_description
+# Build directory: /home/ruslan/catkin_ws/build/xarm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
